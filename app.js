13ee9cb535c86238123fb0650125f7c340fafd57
Guillermo García <guillermieres97@gmail.com>
2022-11-27 12:01:13 +0100

    implement add player to the current list

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
 "use strict";
 
 // dom manipulation
+// buttons
 const addBtn = document.getElementById("add-btn");
 const restartBtn = document.getElementById("restart");
+// lists
 const currentPlayers = document.getElementById("current");
-const dismissedPlayers = document.getElementById("dismissed");
+const eliminatedPlayers = document.getElementById("dismissed");
 const winner = document.getElementById("winner");
 
 // variables
-const currentArray = [];
+const currentList = [];
+const eliminatedList = [];
 
 // events
+// add player
 addBtn.addEventListener("click", function () {
+  const name = getName();
+
+  if (name.length > 0 && name != null) {
+    currentList.push(name);
+
+    currentList.forEach((el) => {
+      createItem(el);
+    });
+  }
+});
+
+function getName() {
   const name = prompt("Write the name of the player!");
 
-  currentArray.push(name);
+  return name;
+}
 
-  currentArray.forEach((el) => {
-    const div = document.createElement("div");
-    div.classList.add("item");
-    
+function createItem(el) {
+  const item = document.createElement("div");
+  item.classList.add("item");
 
-    const p = document.createElement("p");
-    p.innerHTML = el;
-    const icon = document.createElement("p");
-    icon.innerHTML = "Delete";
+  const textName = document.createElement("p");
+  textName.innerHTML = el;
+  const textDelete = document.createElement("p");
+  textDelete.classList.add("delete");
+  textDelete.innerHTML = "Delete";
 
-    div.appendChild(p);
-    div.appendChild(icon);
+  item.appendChild(textName);
+  item.appendChild(textDelete);
 
-    currentPlayers.appendChild(div);
-  });
-});
+  currentPlayers.appendChild(item);
+}
